d67e7eaa472ba20c880a151b81c04761ee601de3
Bastian Soto <bastian.soto.j@usach.cl>
2021-08-05 22:45:00 -0400

    ejemplos en main

--- src/com/paradigmas/Main.java
+++ src/com/paradigmas/Main.java
@@ -1,11 +1,22 @@
 package com.paradigmas;
 
+import com.paradigmas.TDA.SocialNetwork;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hola mundo");
+        SocialNetwork sn1 = new SocialNetwork("manc ave");
+        sn1.register("usuario1","pass1");
+        sn1.register("usuario2", "pass2");
+        sn1.login("usuario2","pass2");
+        //System.out.println(sn1.toString());
+        // System.out.println("ahora es con logout");
+        // sn1.logout();
+        // System.out.println(sn1.toString());
+
     }
 }
-
-
