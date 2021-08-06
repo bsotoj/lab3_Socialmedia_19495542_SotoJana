2cc62f332c243c73ef9904713d1c5b09b7d47726
Bastian Soto <bastian.soto.j@usach.cl>
2021-08-05 22:42:21 -0400

    Interfaz RedSocial + metodos

---
+++ src/com/paradigmas/TDA/RedSocial.java
@@ -0,0 +1,13 @@
+package com.paradigmas.TDA;
+
+public interface RedSocial {
+    void register(String nombreUsuario, String contrasegna);
+    void login (String nombreUsuario, String constrasegna);
+    void logout();
+    //void post(String tipoPublicacion,String Contenido);
+    //void follow(String nombreUsuarioASeguir);
+    //void share(int idPost);
+    //void visualize();
+
+
+}
